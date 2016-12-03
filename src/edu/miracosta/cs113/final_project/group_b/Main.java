9709e2f8ed09a5a6b3cd29a876cc7ae0c842c613
Boris Nikulin <NikulinBE@gmail.com>
2016-12-03 01:11:07 -0800

    Custom Control now in Main.fxml and GraphOnImage initializes properly

--- src/edu/miracosta/cs113/final_project/group_b/Main.java
+++ src/edu/miracosta/cs113/final_project/group_b/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application
 	@Override
 	public void start(Stage primaryStage) throws IOException
 	{
-		Parent root = FXMLLoader.load(getClass().getResource("view/initial prototype.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("view/Main.fxml"));
 		
 		primaryStage.setScene(new Scene(root));
 		
--- src/edu/miracosta/cs113/final_project/group_b/view/custom_control/GraphOnImage.java
+++ src/edu/miracosta/cs113/final_project/group_b/view/custom_control/GraphOnImage.java
@@ -1,20 +1,44 @@
 
 package edu.miracosta.cs113.final_project.group_b.view.custom_control;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.control.Control;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 
 // TODO make this class genericy with allowing graphs that do not use Point2D but force the addition of a function that when applied on the vertex gives back a Point2D
-public class GraphOnImage extends Control
+public class GraphOnImage extends StackPane
 {
-	@FXML private Group graphGroup;
-	@FXML private ImageView imageView;
-	
+	@FXML private Group		graphGroup;
+	@FXML private ImageView	imageView;
+
 	public GraphOnImage ()
 	{
 		super();
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("GraphOnImage.fxml"));
+		loader.setController(this);
+		loader.setRoot(this);
+
+		try
+		{
+			loader.load();
+		}
+		catch (IOException exception)
+		{
+			throw new RuntimeException(exception);
+		}
+	}
+
+	@FXML
+	void initialize()
+	{
 		
+		Circle circle = new Circle(10, 10, 5);
+		graphGroup.getChildren().add(circle);
 	}
 }
