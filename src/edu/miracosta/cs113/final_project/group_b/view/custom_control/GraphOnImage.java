03ff6453e5b5041402f3256b56c904d36d692e48
Boris Nikulin <NikulinBE@gmail.com>
2016-12-04 13:21:53 -0800

    Figured out solution to keeping relative coordinates the same
    When resizing, the pane does not layout it's children. Thus after resizing, the children may not be over the same place as originally. This will not work since the circles and lines must stay over the same place in the image no matter what. Thus I finally used low level binding api to bind the circle x and y to a calculated and correct new x and y based on the initial bounds of the image. The initial x and y will be supplied by added vertices. A better solution now apparent to me, is to extend the Pane class and manage the children's layout since this is a problem of layout, not a problem of where to position.

--- src/edu/miracosta/cs113/final_project/group_b/view/custom_control/GraphOnImage.java
+++ src/edu/miracosta/cs113/final_project/group_b/view/custom_control/GraphOnImage.java
@@ -3,8 +3,10 @@ package edu.miracosta.cs113.final_project.group_b.view.custom_control;
 
 import java.io.IOException;
 
+import javafx.beans.binding.DoubleBinding;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Bounds;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -21,6 +23,8 @@ public class GraphOnImage extends StackPane
 {
 	@FXML private Pane						graphPane;
 	@FXML private ResizeToParentImageView	imageView;
+	
+	private Bounds initBounds;
 
 	public GraphOnImage ()
 	{
@@ -57,9 +61,50 @@ public class GraphOnImage extends StackPane
 			graphPane.getChildren().add(circle1);
 			graphPane.getChildren().add(circle2);
 		}
-		
+
+		initBounds = imageView.getBoundsInLocal();
+		double xyRatio = initBounds.getWidth() / initBounds.getHeight();
+		System.out.println(initBounds);
+
 		Circle circleStart = new Circle(0, 0, 5, Color.GREEN);
 		Circle circleEnd = new Circle(200, 200, 5, Color.RED);
+		
+//		DoubleBinding dbx = Bindings.multiply(200, EasyBind.map(imageView.boundsInLocalProperty(), Bounds::getWidth));
+//		Bindings.divide(EasyBind.map(imageView.boundsInLocalProperty(), Bounds::getWidth), initBounds.getWidth());
+//		EasyBind.map(imageView.boundsInLocalProperty(), Bounds::getWidth);
+		
+		DoubleBinding endX = new DoubleBinding()
+		{
+			{
+				super.bind(imageView.boundsInLocalProperty());
+			}
+			@Override
+			protected double computeValue()
+			{
+				return 200 * imageView.getBoundsInLocal().getWidth() / initBounds.getWidth();
+			}
+		};
+		
+		DoubleBinding endY = new DoubleBinding()
+		{
+			{
+				super.bind(imageView.boundsInLocalProperty());
+			}
+			@Override
+			protected double computeValue()
+			{
+				return 200 * imageView.getBoundsInLocal().getHeight() / initBounds.getHeight();
+			}
+		};
+		
+		System.out.println(circleEnd);
+//		imageView.boundsInLocalProperty().addListener((e) -> {
+//			circleEnd.setCenterX(200 * imageView.getBoundsInLocal().getWidth() / initBounds.getWidth());
+//			circleEnd.setCenterY(200 * imageView.getBoundsInLocal().getHeight() / initBounds.getHeight());
+//		});
+		circleEnd.centerXProperty().bind(endX);
+		circleEnd.centerYProperty().bind(endY);
+		System.out.println(circleEnd);		
 		graphPane.getChildren().add(circleStart);
 		graphPane.getChildren().add(circleEnd);
 	}
