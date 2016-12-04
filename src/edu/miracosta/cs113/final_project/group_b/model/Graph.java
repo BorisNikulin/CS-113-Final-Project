cceb72160a52b5de3af66a1386d5ae493025b3e5
Marina Mizar <m.mizar@hotmail.com>
2016-12-03 23:16:17 -0800

    Changed & moved Graph class
    Modified graph class to hold generic type vertices (will include
    coordinate values for A* algorithm). Made array of vertices' adjacency
    lists into a map. Moved class to model package.

--- src/edu/miracosta/cs113/final_project/group_b/model/Graph.java
+++ src/edu/miracosta/cs113/final_project/group_b/model/Graph.java
@@ -1,18 +1,20 @@
-package edu.miracosta.cs113.final_project.group_b.model.test;
+package edu.miracosta.cs113.final_project.group_b.model;
 
+import java.util.HashMap;
 import java.util.Iterator;
 
 /**
- * Graph.java: Implementation of a weighted, non-directional graph class
- * with inner Edge class. Includes methods to add, check for, and remove
- * edges. Maintains an adjacency list (Array of LinkedLists of Edges).
+ * Graph.java: Implementation of a weighted, non-directional graph class 
+ * with inner Edge class. Vertex class type is generic. Includes methods 
+ * to add, check for, and remove edges. Maintains a map of adjacency lists
+ * (maps vertex of type E to list of edges (LinkedList<Edge>)).
  * 
  * @author Marina Mizar
  */
 
 import java.util.LinkedList;
 
-public class Graph {
+public class Graph<E> {
 	
 	/**
 	 * Stores information about the edges that link vertices, including
@@ -20,8 +22,8 @@ public class Graph {
 	 */
 	private class Edge {
         
-        private int source;
-        private int destination;
+        private E source;
+        private E destination;
         private double weight;
         
         /**
@@ -33,7 +35,7 @@ public class Graph {
          * @param destination
          * 		  Second connected vertex
          */
-        public Edge(int source, int destination) {
+        public Edge(E source, E destination) {
             this.source = source;
             this.destination = destination;
             this.weight = 1.0;
@@ -49,7 +51,7 @@ public class Graph {
          * @param weight
          * 		  Comparative value of edge
          */
-        public Edge(int source, int destination, double weight) {
+        public Edge(E source, E destination, double weight) {
             this.source = source;
             this.destination = destination;
             this.weight = weight;
@@ -58,9 +60,9 @@ public class Graph {
         /**
          * Accessor for source vertex.
          * 
-         * @return Integer value representing a vertex
+         * @return Value representing a vertex
          */
-        public int getSource() {
+        public E getSource() {
             return source;
         }
          
@@ -69,7 +71,7 @@ public class Graph {
          * 
          * @return Integer value representing a vertex
          */
-        public int getDestination() {
+        public E getDestination() {
             return destination;
         }
          
@@ -102,7 +104,8 @@ public class Graph {
          * @return True if other object is an Edge with same
          *         source and destination values.
          */
-        @Override
+        @SuppressWarnings("unchecked")
+		@Override
         public boolean equals(Object o) {
             if (o == null) {
                 return false;
@@ -130,46 +133,52 @@ public class Graph {
         }
     }
      
-    private int vertices;             // Number of vertices
-    private LinkedList<Edge>[] edges; // Adjacency list
+    private HashMap<E, LinkedList<Edge>> edges; // Adjacency list
+    private int vertices;                             // Number of vertices
      
     /**
-     * Creates a Graph with default minimum number of vertices (1).
+     * Default constructor; creates a Graph object.
      */
-    @SuppressWarnings("unchecked")
-	public Graph() {
-        vertices = 1;
-        edges = (LinkedList<Edge>[]) new LinkedList[vertices];
+    public Graph() {
+        edges = new HashMap<E, LinkedList<Edge>>();
+        vertices = 0;
     }
-     
+    
     /**
-     * Creates a Graph with specified number of vertices (does not
-     * allow zero or negative number).
+     * Adds vertex to graph by putting it in the map that links
+     * vertices to edges. Increments vertex counter.
      * 
-     * @param vertices
-     * 		  Number of nodes within data structure
+     * @param vertex
+     *        Vertex object to add to graph
      */
-    @SuppressWarnings("unchecked")
-	public Graph(int vertices) {
-        if (vertices > 0) {
-            this.vertices = vertices;
-        }
-        else {
-            vertices = 1;
-        }
-        edges = (LinkedList<Edge>[]) new LinkedList[vertices];
+    public void addVertex(E vertex) {
+    	// Add vertex + its initialized adjacency list
+    	edges.put(vertex, new LinkedList<Edge>());
+    	vertices++;
     }
-     
+    
     /**
-     * Accessor for number of vertices.
+     * Removes a vertex from the graph, as well as all the edges
+     * connected to it.
      * 
-     * @return Integer representing number of vertices (with indices
-     *         from 0 to number - 1) contained within Graph
+     * @param vertex
+     *        Vertex to remove from graph
      */
-    public int getVertices() {
-        return vertices;
+    public void removeVertex(E vertex) {
+    	// Get vertex's adjacency list
+    	LinkedList<Edge> adjacencyList = edges.remove(vertex);
+    	
+    	// If list contains edges, remove them from graph
+    	if (!adjacencyList.isEmpty()) {
+    		for (Edge edge : adjacencyList) {
+    			removeEdge(edge.destination, vertex);
+    		}
+    	}
+    	
+    	// Then remove vertex from map
+    	edges.remove(vertex);
     }
-     
+
     /**
      * Inserts an edge linking two vertices by adding new Edge object
      * to two LinkedLists, one for source and one for destination.
@@ -180,23 +189,26 @@ public class Graph {
      * 		  Ending vertex
      * @param weight
      * 		  Value of edge
-     * @return True if insertion is successful (edge has valid source &
-     *         destination and doesn't already exist in Graph)
+     * @return True if insertion is successful (edge doesn't already
+     *         exist in Graph)
      */
-    public boolean addEdge(int source, int destination, double weight) {
-        // Check to make sure vertices are valid for graph
-        if (invalidVertex(source) | invalidVertex(destination)) {
-            return false;
-        }
-         
+    public boolean addEdge(E source, E destination, double weight) {
+    	LinkedList<Edge> sourceList = edges.get(source.hashCode());
+    	LinkedList<Edge> destList = edges.get(source.hashCode());
+    	
+    	// If one or both vertices do not exist:
+    	if (sourceList == null || destList == null) {
+    		return false;
+    	}
+    	
         // Check to make sure edge doesn't already exist
         if (!isEdge(source, destination)) {
         	return false;
         }
-         
+        
         // Graph is not directional, add edge to both lists
-        edges[source].add(new Edge(source, destination, weight));
-        edges[source].add(new Edge(destination, source, weight));
+        sourceList.add(new Edge(source, destination, weight));
+        destList.add(new Edge(destination, source, weight));
          
         return true;
     }
@@ -208,24 +220,26 @@ public class Graph {
      * 		  Start vertex
      * @param destination
      * 		  End vertex
-     * @return True if edge is valid, existed in Graph, and was
-     *         successfully removed
+     * @return True if edge existed in Graph and was successfully removed
      */
-    public boolean removeEdge(int source, int destination) {
-    	// Check to make sure vertices are valid for graph
-        if (invalidVertex(source) | invalidVertex(destination)) {
-            return false;
-        }
-        
-        // Graph is not directional, remove edge from both lists
-        return edges[source].remove(new Edge(source, destination)) &&
-        		edges[destination].remove(new Edge(destination, source));
+    public boolean removeEdge(E source, E destination) {
+    	LinkedList<Edge> sourceList = edges.get(source.hashCode());
+    	LinkedList<Edge> destList = edges.get(source.hashCode());
+    	
+    	// If one or both vertices do not exist:
+    	if (sourceList == null || destList == null) {
+    		return false;
+    	}
+    	
+    	// Graph is not directional, remove edge from both lists
+        return sourceList.remove(new Edge(source, destination)) &&
+        		destList.remove(new Edge(destination, source));
     }
     
     /**
      * Initializes LinkedList for each vertex if it does not
      * already exist, otherwise checks whether LinkedList at source
-     * index contains the specified edge.
+     * contains the specified edge.
      * 
      * @param source
      * 		  Start vertex
@@ -234,24 +248,17 @@ public class Graph {
      * @return True if vertices each have a LinkedList of edges and
      * 	       source list contains Edge
      */
-    public boolean isEdge(int source, int destination) {
-    	boolean listsExist = true;
-    	// Need to initialize list of edges for node if not already done
-        if (edges[source] == null) {
-            edges[source] = new LinkedList<Edge>();
-            listsExist = false;
-        }
-        if (edges[destination] == null) {
-            edges[destination] = new LinkedList<Edge>();
-            listsExist = false;
-        }
+    public boolean isEdge(E source, E destination) {
+    	LinkedList<Edge> sourceList = edges.get(source.hashCode());
+    	LinkedList<Edge> destList = edges.get(source.hashCode());
+    	
+    	// If one or both vertices do not exist:
+    	if (sourceList == null || destList == null) {
+    		return false;
+    	}
         
-        if (listsExist) {
-        	return edges[source].contains(new Edge(source, destination));
-        }
-        
-        // One of the lists for a vertex was not initialized, edge cannot exist
-    	return false; 
+    	// Check adjacency list for edge
+    	return sourceList.contains(new Edge(source, destination));
     }
     
     /**
@@ -259,14 +266,15 @@ public class Graph {
      * matching Edge object, which includes weight data.
      * 
      * @param source
-     * 		  Number of start vertex
+     * 		  Start vertex
      * @param destination
-     * 		  Number of destination vertex
+     * 		  Destination vertex
      * @return The edge linked the specified vertices, or null if not found
      */
-    public Edge getEdge(int source, int destination) {
+    public Edge getEdge(E source, E destination) {
+    	LinkedList<Edge> sourceList = edges.get(source.hashCode());
     	Edge target = new Edge(source, destination);
-    	for (Edge edge : edges[source]) {
+    	for (Edge edge : sourceList) {
     		if (edge.equals(target)) {
     			return edge;
     		}
@@ -281,22 +289,8 @@ public class Graph {
      * 	      Index of vertex over whose edges to iterate
      * @return An Iterator<Edge> object
      */
-    public Iterator<Edge> edgeIterator(int vertex) {
-    	return edges[vertex].iterator();
-    }
-     
-    /**
-     * Ensures vertex index given is in the range of zero to one less
-     * than number of vertices.
-     * 
-     * @param vertex
-     * 		  Integer representing index of vertex
-     * @return True if vertex is invalid, false if OK
-     */
-    private boolean invalidVertex(int vertex) {
-        if (vertex < 0 || vertex >= vertices) {
-            return true;
-        }
-        return false;
+    public Iterator<Edge> edgeIterator(E vertex) {
+    	LinkedList<Edge> edgeList = edges.get(vertex.hashCode());
+    	return edgeList.iterator();
     }
 }
