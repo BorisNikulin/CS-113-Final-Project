c416f8cea4317dde7dc40b6ccaf07a23bb39adca
Boris Nikulin <NikulinBE@gmail.com>
2016-12-06 17:21:49 -0800

    Made initBounds into a property as well as computed ratios
    Dependencies are more clearly and accurately expressed. The ratio values should also be only computed in one place and then referenced.

--- src/edu/miracosta/cs113/final_project/group_b/view/custom_control/GraphOnImage.java
+++ src/edu/miracosta/cs113/final_project/group_b/view/custom_control/GraphOnImage.java
@@ -4,6 +4,10 @@ package edu.miracosta.cs113.final_project.group_b.view.custom_control;
 import java.io.IOException;
 
 import javafx.beans.binding.DoubleBinding;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Bounds;
@@ -23,8 +27,10 @@ public class GraphOnImage extends StackPane
 {
 	@FXML private Pane						graphPane;
 	@FXML private ResizeToParentImageView	imageView;
-	
-	private Bounds initBounds;
+
+	private ObjectProperty<Bounds>			initBounds	= new SimpleObjectProperty<Bounds>(this, "initBounds");
+	private DoubleProperty					widthRatio	= new SimpleDoubleProperty(this, "widthRatio");
+	private DoubleProperty					heightRatio	= new SimpleDoubleProperty(this, "heightRatio");
 
 	public GraphOnImage ()
 	{
@@ -47,23 +53,92 @@ public class GraphOnImage extends StackPane
 	@FXML
 	void initialize()
 	{
-//		// EasyBind.when(EasyBind.map(sceneProperty(), sp -> sp !=
-//		// null)).bind(graphGroup.he, this.heightProperty());
-//		System.out.println(imageView);	
+		imageView.imageProperty().addListener(e -> initBounds.set(imageView.getBoundsInLocal()));
+		
 		imageView.setImage(new Image("file:/D:/Documents/Script%20Files/Java/CS%20113/final_project/src/edu/miracosta/cs113/final_project/group_b/view/custom_control/UTm4Sg.jpg"));
-//		System.out.println(imageView);
-//		System.out.println(imageView.getImage().getProgress());
-//
-		for (int i = 1; i <= 200; i += 10)
+		
+		DoubleBinding widthRatioBinding = new DoubleBinding()
 		{
+			{
+				super.bind(initBounds, imageView.boundsInLocalProperty());
+			}
+			
+			@Override
+			protected double computeValue()
+			{
+				return imageView.getBoundsInLocal().getWidth() / initBounds.get().getWidth();
+			}
+		};
+		
+		DoubleBinding heightRatioBinding = new DoubleBinding()
+		{
+			{
+				super.bind(initBounds, imageView.boundsInLocalProperty());
+			}
+			
+			@Override
+			protected double computeValue()
+			{
+				return imageView.getBoundsInLocal().getHeight() / initBounds.get().getHeight();
+			}
+		};
+		
+		widthRatio.bind(widthRatioBinding);
+		heightRatio.bind(heightRatioBinding);
+		
+
+		for (int i = 1; i <= initBounds.get().getHeight(); i += 1)
+		{
+			final double x1 = i;
+			final double y1 = i;
+			final double x2 = initBounds.get().getWidth() - i;
 			Circle circle1 = new Circle(i, i, 5,Color.YELLOW);
-			Circle circle2 = new Circle(100 - i, i, 5, Color.CYAN);
+			Circle circle2 = new Circle(500 - i, i, 5, Color.CYAN);
+			
+			DoubleBinding circleBind1x = new DoubleBinding()
+			{
+				{
+					super.bind(imageView.boundsInLocalProperty(), widthRatio);
+				}
+				@Override
+				protected double computeValue()
+				{
+					return x1 * widthRatio.doubleValue();
+				}
+			};
+			DoubleBinding circleBind1y = new DoubleBinding()
+			{
+				{
+					super.bind(imageView.boundsInLocalProperty(), heightRatio);
+				}
+				@Override
+				protected double computeValue()
+				{
+					return y1 * heightRatio.doubleValue();
+				}
+			};
+			DoubleBinding circleBind2x = new DoubleBinding()
+			{
+				{
+					super.bind(imageView.boundsInLocalProperty(), widthRatio);
+				}
+				@Override
+				protected double computeValue()
+				{
+					return x2 * widthRatio.doubleValue();
+				}
+			};
+
+			circle1.centerXProperty().bind(circleBind1x);
+			circle1.centerYProperty().bind(circleBind1y);
+			circle2.centerXProperty().bind(circleBind2x);
+			circle2.centerYProperty().bind(circleBind1y);
+			
 			graphPane.getChildren().add(circle1);
 			graphPane.getChildren().add(circle2);
 		}
 
-		initBounds = imageView.getBoundsInLocal();
-		double xyRatio = initBounds.getWidth() / initBounds.getHeight();
+
 		System.out.println(initBounds);
 
 		Circle circleStart = new Circle(0, 0, 5, Color.GREEN);
@@ -76,24 +151,24 @@ public class GraphOnImage extends StackPane
 		DoubleBinding endX = new DoubleBinding()
 		{
 			{
-				super.bind(imageView.boundsInLocalProperty());
+				super.bind(imageView.boundsInLocalProperty(), widthRatio);
 			}
 			@Override
 			protected double computeValue()
 			{
-				return 200 * imageView.getBoundsInLocal().getWidth() / initBounds.getWidth();
+				return 200 * widthRatio.doubleValue();
 			}
 		};
 		
 		DoubleBinding endY = new DoubleBinding()
 		{
 			{
-				super.bind(imageView.boundsInLocalProperty());
+				super.bind(imageView.boundsInLocalProperty(), heightRatio);
 			}
 			@Override
 			protected double computeValue()
 			{
-				return 200 * imageView.getBoundsInLocal().getHeight() / initBounds.getHeight();
+				return 200 * heightRatio.doubleValue();
 			}
 		};
 		
