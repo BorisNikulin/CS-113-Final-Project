6882dac8ce7088756a786d3cac87c5e1beca84f5
Marina Mizar <m.mizar@hotmail.com>
2016-12-09 17:14:16 -0800

    Created step version of AStar and fixed a broken test method

--- src/edu/miracosta/cs113/final_project/group_b/model/AStar.java
+++ src/edu/miracosta/cs113/final_project/group_b/model/AStar.java
@@ -11,9 +11,11 @@ import java.util.function.ToDoubleBiFunction;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.MapProperty;
+import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleMapProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 
 /**
@@ -28,49 +30,85 @@ public class AStar<V> {
 	
 	private PriorityQueue<VertexPriority<V>> frontier; // Vertices not yet evaluated
 	private Map<V, Double> costSoFar;                  // Pairs vertices with their total path costs from start
-	private MapProperty<V, V> cameFrom = new SimpleMapProperty<>(this, "cameFrom", FXCollections.observableHashMap());                        // Pairs vertices with their immediate predecessor
+	private MapProperty<V, V> cameFrom;                // Pairs vertices with their immediate predecessor
 	private Graph<V> graph;                            // Graph to find path from start to goal
-	private ToDoubleBiFunction<V, V> heuristic;
-	private DoubleProperty sleep = new SimpleDoubleProperty(this, "sleep", 0);
-//	private ReadOnlyObjectProperty<V> start = new SimpleReadOnlyObjectProperty();
-	private BooleanProperty isPlay = new SimpleBooleanProperty(this, "isPlay", false);
-	private TimeUnit timeUnit = TimeUnit.MILLISECONDS;
-	
-	{
-		try {
-			Thread.sleep(timeUnit.toMillis((long) sleep.get()));
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			
-		}
-		
-		
-		// or no time unit and assume milliseconds
-	}
+	private ToDoubleBiFunction<V, V> heuristic;        // Heuristic function used to predict next best vertex
+	private DoubleProperty sleep;                      // Determines time to pause between steps
+	private ReadOnlyObjectProperty<V> start;           // Start vertex
+	private ReadOnlyObjectProperty<V> goal;            // End vertex
+	private BooleanProperty isPlay;                    // True if steps are being animated in view (play button activated)
+	private TimeUnit timeUnit;                         // Determines time unit given to sleep
 	
 	/**
-	 * Initializes all data structures needed for AStar algorithm.
+	 * Initializes all data structures needed for AStar algorithm,
+	 * including Properties used to bind view to model.
 	 * 
 	 * @param graph
 	 *        Data structure containing all vertices possible for path
 	 * @param start
 	 *        Beginning vertex from which to find path
+	 * @param goal
+	 *        End vertex to seek path towards
+	 * @param heuristic
+	 *        Heuristic function that calculates best next vertex for path,
+	 *        based on position in coordinate system
 	 */
-	public AStar(Graph<V> graph, V start, ToDoubleBiFunction<V, V> heuristic) {
+	public AStar(Graph<V> graph, V start, V goal, ToDoubleBiFunction<V, V> heuristic) {
 		this.heuristic = heuristic;
 		this.graph = graph;
+		this.start = new SimpleObjectProperty<V>(this, "start", start);
+		this.goal = new SimpleObjectProperty<V>(this, "goal", goal);
+	
+		isPlay = new SimpleBooleanProperty(this, "isPlay", false);
 		costSoFar = new HashMap<V, Double>();
-//		cameFrom = new HashMap<V, V>();
+		cameFrom = new SimpleMapProperty<>(this, "cameFrom", FXCollections.observableHashMap());
 		frontier = new PriorityQueue<VertexPriority<V>>();
-		cameFrom.get().put(start, null);             // Add first vertex (no predecessor)
-		costSoFar.put(start, 0.0);             // Add first vertex (no path cost yet)
-		frontier.add(new VertexPriority<V>(start, 0));
+		cameFrom.get().put(start, null);               // Add first vertex (no predecessor)
+		costSoFar.put(start, 0.0);                     // Add first vertex (no path cost yet)
+		frontier.add(new VertexPriority<V>(start, 0)); // Initialize frontier with start vertex
+	}
+	
+	/**
+	 * Performs one "step" of A* algorithm, where an item from the
+	 * frontier is chosen and its adjacent vertices are evaluated.
+	 * Step ends when a new vertex selected to be added to the frontier.
+	 */
+	public void step() {
+		VertexPriority<V> current;                                // Current vertex in frontier being evaluated
+		LinkedList<Edge<V>> adjacentEdges;                        // All edges adjacent to current vertex
+		double newCost, priority;                                 // Calculated cost between current & next; current priority
+		V next;                                                   // Next adjacent vertex to current
+		boolean vertexAdded = false;                              // Flag: has a new vertex been added to path?
+		 
+		while (!frontier.isEmpty() && !vertexAdded) {             // Continue evaluating vertices until one is added to path
+			current = frontier.poll();                            // Get best new vertex from priority queue
+			
+			if (current.getVertex().equals(goal.get())) {
+				return;                                           // End search early once goal has been reached
+			}
+
+			adjacentEdges = graph.getEdges(current.getVertex());
+			
+			for (Edge<V> edge : adjacentEdges) {
+				next = edge.getDestination();                      // Get next adjacent edge & cost between current & next
+				newCost = costSoFar.get(current.getVertex()) + edge.getWeight();
+				
+				if (!costSoFar.containsKey(next) ||                // If cost has not been found for next vertex 
+					newCost < costSoFar.get(next)) {               // Or new calculated cost is smaller
+					costSoFar.put(next, newCost);                  // Replace cost with newer, smaller cost
+					                                               // Calculate priority for next vertex, add to frontier
+					priority = newCost + heuristic.applyAsDouble(goal.get(), next);					
+					frontier.offer(new VertexPriority<V>(next, priority));
+					cameFrom.get().put(next, current.getVertex()); // Record next vertex's predecessor (current vertex)
+					vertexAdded = true;                            // Now step will end after rest of adjacent vertices evaluated
+				}
+			}
+		}
 	}
 	
 	/**
 	 * Performs AStar path-finding algorithm from start vertex to
-	 * given goal vertex.
+	 * given goal vertex. Not used in GUI, just for testing purposes.
 	 * 
 	 * @param goal
 	 *        Ending vertex
@@ -100,7 +138,7 @@ public class AStar<V> {
 					                                            // Calculate priority for next vertex, add to frontier
 					priority = newCost + heuristic.applyAsDouble(goal, next);					
 					frontier.offer(new VertexPriority<V>(next, priority));
-					cameFrom.put(next, current.getVertex());    // Record next vertex's predecessor (current vertex)
+					cameFrom.get().put(next, current.getVertex());// Record next vertex's predecessor (current vertex)
 				}
 			}
 		}
@@ -128,16 +166,51 @@ public class AStar<V> {
 		System.out.print(goal);
 	}
 	
+	/**
+	 * Puts the current thread to sleep for time given.
+	 * 
+	 * @param time
+	 *        Double representing amount of time to sleep
+	 * @param unit
+	 *        Specifies the unit of measurement (seconds, etc.)
+	 */
+	public void goToSleep(double time, TimeUnit unit) {
+		timeUnit = unit;
+		sleep = new SimpleDoubleProperty(this, "sleep", time);
+		try {
+			Thread.sleep(timeUnit.toMillis((long) sleep.get()));
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();			
+		}
+	}
+	
+	/**
+	 * Property wrapper for sleep amount; used in controller.
+	 * 
+	 * @return Sleep property object
+	 */
 	public DoubleProperty sleepProperty()
 	{
 		return sleep;
 	}
 	
+	/**
+	 * Gets sleep value as a double.
+	 * 
+	 * @return Sleep as a double
+	 */
 	public final double getSleep()
 	{
 		return sleep.doubleValue();
 	}
 	
+	/**
+	 * Sets sleep to specified double value.
+	 * 
+	 * @param value
+	 *        Number of time units to sleep.
+	 */
 	public final void setDouble(double value)
 	{
 		sleep.set(value);
